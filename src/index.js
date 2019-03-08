556f574ded82fc12510d0afdbcaba9316cf9e45a
SankalpSh <sankalpsh98@gmail.com>
2019-03-08 21:39:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import ContactAddress from './ContactAddress';
+import Hello from './Hello';
 import './style.css';
 
-class App extends Component {
+class Tooltip extends React.Component {
   constructor(props) {
-    super(props);
-    this.handleMouseHover = this.handleMouseHover.bind(this);
+    super(props)
+
     this.state = {
-      items: [],
-      isloaded: false,
-      isHovering: false,
+      displayTooltip: false
     }
+    this.hideTooltip = this.hideTooltip.bind(this)
+    this.showTooltip = this.showTooltip.bind(this)
+  }
+  
+  hideTooltip () {
+    this.setState({displayTooltip: false})
     
   }
+  showTooltip () {
+    this.setState({displayTooltip: true})
+  }
 
-  handleMouseHover() {
+  render() {
+    let message = this.props.message
+    let position = this.props.position
+    return (
+      <span className='tooltip'
+          onMouseLeave={this.hideTooltip}
+        >
+        {this.state.displayTooltip &&
+        <div className={`tooltip-bubble tooltip-${position}`}>
+          <div className='tooltip-message'>{message}</div>
+        </div>
+        }
+        <span 
+          className='tooltip-trigger'
+          onMouseOver={this.showTooltip}
+          >
+          {this.props.children}
+        </span>
+      </span>
+    )
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.handleMouseHover = this.handleMouseHover.bind(this);
+    this.state = {
+      items:[],
+      isLoaded:false,
+      isHovering:false
+    };
+  }
+
+    handleMouseHover() {
     this.setState(this.toggleHoverState);
   }
 
@@ -24,63 +65,60 @@ class App extends Component {
       isHovering: !state.isHovering,
     };
   }
-  
+
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
     .then(json => {
-      this.setState({
-        isloaded: true,
+      setTimeout(()=>{
+        this.setState({
+        isLoaded: true,
         items: json,
-      })
+      });
+      },1500)
+      
     });
   }
 
-
   render() {
-    var { isloaded, items,isHovering} = this.state;
-    if(!isloaded)
-    {
-      return<div>
-        <h1><strong>Users(Loading)</strong></h1>
-      </div>
-    }
-    
-
-    else {
+    var { isLoaded, items} = this.state;
+    if(!isLoaded){
       return (
-        
       <div>
-        <h1>Users</h1>
-        <div onMouseEnter={this.handleMouseHover}
-          onMouseLeave={this.handleMouseHover} className="mainBorder">
-           {items.map(item => (
-            <div key = {item.id}>
-              <p><strong className="name">{item.name}</strong>
-              <span>({item.username})</span>
-              </p>
-              <div>
-                {
-                 this.state.isHovering &&
-                 <span style={{ border: '1px solid black'}}>
-                  
-                 <div className="box">
-                 
-                  <h4><strong className="dark">Street</strong> {item.address.street}</h4>
-                  <h4><strong className="dark">Suite</strong> {item.address.suite}</h4>
-                  <h4><strong className="dark">City</strong> {item.address.city}</h4>
-                  <h4><strong className="dark">Zip</strong> {item.address.zipcode}</h4>
-                  </div>
-                  </span> 
-                 }
-              </div>
-            </div>
-          ))}
-        </div>
+          <h1>User(Loading)</h1>
       </div>
     );
     }
-    
+    else{
+      return(
+        <div>
+        <h1>Users</h1>
+        <div className="mainBorder">
+        {
+          items.map(item =>(
+            <div key = {item.id}>
+              <p><strong onMouseEnter={this.handleMouseHover}
+                         onMouseLeave={this.handleMouseHover} 
+                         className="name"><Tooltip message={this.state.isHovering &&
+                 <span >
+                  
+                 <ul className="box">
+                 
+                  <li><strong className="dark">Street</strong> {item.address.street}</li>
+                  <li><strong className="dark">Suite</strong> {item.address.suite}</li>
+                  <li><strong className="dark">City</strong> {item.address.city}</li>
+                  <li><strong className="dark">Zip</strong> {item.address.zipcode}</li>
+                  </ul>
+                  </span> 
+              } position={'right'}>{item.name}</Tooltip> </strong><span className="username">({item.username})</span></p>
+              
+            </div>
+          ))
+        }
+        </div>
+      </div>
+      );  
+    }
   }
 }
 
